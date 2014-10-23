f12c33215da43918ade383907b018bb30444eff0
cargo74 <cargo74@yahoo.com.au>
2014-10-23 10:05:44 +1000

    g maps support added to helper.js

--- js/helper.js
+++ js/helper.js
@@ -89,7 +89,7 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+	console.log(loc.pageX, loc.pageY)
 });
 
 
@@ -141,7 +141,7 @@ function initializeMap() {
     for (var job in work.jobs) {
       locations.push(work.jobs[job].location);
     }
-
+	console.log(locations);
     return locations;
   }
 
@@ -233,15 +233,15 @@ function initializeMap() {
 };
 
 /*
-Uncomment all the code below when you're ready to implement a Google Map!
+Uncomment all  the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window 
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
